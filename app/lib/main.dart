89a8b960c5d1cb2200aab4357a7b4490744ceca7
yukiko laurentiu <yuki.laurentiu@gmail.com>
2023-02-25 18:53:15 -0800

    set up flutter

---
+++ app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State createState() => _State();
+}
+
+class _State extends State<MyApp>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bar'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: const <Widget>[
+              Text('Hello'),
+              Text(''),
+            ],
+          )),
+
+      ),
+    );
+  }
+}
