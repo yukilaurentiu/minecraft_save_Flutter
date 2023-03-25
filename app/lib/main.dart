aacd8e9efdfb081e5b29377234dd9cef7cebe5c5
yukiko laurentiu <yuki.laurentiu@gmail.com>
2023-03-24 18:40:56 -0700

    separate init class

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mcsaver_package/mcsaver_package.dart';
+import 'package:app/init.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,24 +22,40 @@ class _State extends State<MyApp>{
   var menu = Menu();
   
 
+  // Future<String> getUsbs() async {
+  //   var result = await Process.run('ls', ['/media/yuki/']);
+  //   String usbs = result.stdout;
+  //   return usbs;
+  // }
+
+
   @override
   Widget build(BuildContext context) {
-    menu.init();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bar'),
+        title: const Text('Mincraft Saver'),
       ),
       body: Container(
         padding: const EdgeInsets.all(32.0),
         child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('Hello'),
-              Text("${menu.stepCheckUsb()}"),
-            ],
-          )),
-
-      ),
+          child: Init(menu),
+        )
+      )
     );
   }
 }
+
+     // return FutureBuilder<int>(
+                //   future: menu.stepCheckUsb(),
+                //   builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                //     if (snapshot.hasData) {
+                //       return Text('${menu.data.usbs}');
+                //     } 
+                //     else if (snapshot.hasError) {
+                //       return Text("${snapshot.error}");
+                //     }
+                //     else {
+                //       return const CircularProgressIndicator();
+                //     }
+                //   }
+                // );
